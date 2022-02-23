01dace3cab739116334e16c20c48914b9b2a6e83
riyashuklaa <86917681+riyashuklaa@users.noreply.github.com>
2022-02-23 03:02:39 -0800

    Add files via upload

---
+++ scratch_canvas.py
@@ -0,0 +1,27 @@
+import turtle
+turtle.pensize(1)
+turtle.penup()
+turtle.forward(150)
+turtle.pendown()
+turtle.left(36)
+for i in range(5):
+    turtle.forward(150)
+    turtle.left(144)
+turtle.left(100)
+turtle.penup()
+turtle.forward(125)
+turtle.pendown()
+turtle.left(45)
+for i in range(5):
+    turtle.forward(100)
+    turtle.left(144)
+
+turtle.penup()
+turtle.forward(150)
+turtle.pendown()
+for i in range(5):
+    turtle.forward(50)
+    turtle.left(144)
+
+
+
